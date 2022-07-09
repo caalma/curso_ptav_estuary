604ee434d0cd184bdc67daeba5cfb0c203e4e956
caalma <nds.suscript@gmail.com>
2022-07-09 01:27:33 -0300

    Agregado de utilidades

---
+++ utiles/codigos_y_sugerencias.md
@@ -0,0 +1,30 @@
+# Automatizaciones útiles
+
+## Secuencia a video
+
+	ffmpeg -f image2 -pattern_type glob -i 'p*.jpg' -b 8000k -r 25 ../naranjo.mp4
+
+	ffmpeg -f image2 -pattern_type glob -i './png/*.png' -b 8000k -r 25 ./$(basename $(pwd)).mov
+
+## Secuencia a gif
+
+	convert png_/*.png $(basename $(pwd)).gif
+
+
+## Procesar muchas carpetas
+
+	raiz=$(pwd); for carpeta in *;do cd $carpeta; echo $(pwd); imagen_reducir 1000;  cd $raiz; done
+
+
+	raiz=$(pwd); for carpeta in *;do cd $carpeta; echo $(pwd); ffmpeg -f image2 -pattern_type glob -i './*.jpg' -b 8000k -r 6 ../$(basename $(pwd)).mov; cd $raiz; done
+
+
+## Obtener las característica de todos los audios/videos desde la carpeta actual
+	AT=/tmp/x; for AU in $(find -type f); do ffprobe $AU 2> $AT && grep 'Stream' $AT;done > tipos_de_samples.txt
+
+
+## Generar videos transparentes con kdenlive
+
+1 - Elegir el video a usar y aplicarle la transparencia por efecto de transformación o por imagen png transparente.
+2 - Configurar el "Modo de Composición" (en las utilidades del bloque de pistas).
+3 - Exportar eligiendo desde el grupo de "Videos transparentes" entre : mov, webm (vp9).
---
+++ utiles/crear_json_de_samples.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+from sys import argv
+from os import listdir
+from os.path import exists, isdir, isfile, splitext, join
+import json
+
+texto_ayuda = """
+Ejemplo de uso:
+
+{archivo_script} ../audio/ archivo.json
+
+Requiere que se le indique un ruta de carpeta.
+Dicha ruta debe contener carpetas con archivos para samples.
+Opcionalmente puede indicarsele un nombre de archivo para grabar el json. En caso de no hacerlo el nombre por defecto es samples.json .
+Los formatos permitidos para samples son: {formatos_permitidos} .
+
+"""
+
+formatos_permitidos = {
+    'bmp': 'image',
+    'gif': 'image',
+    'ico': 'image',
+    'jpg': 'image',
+    'jpeg': 'image',
+    'png': 'image',
+    'svg': 'image',
+    'webp': 'image',
+
+    'wav': 'audio',
+    'mp3': 'audio',
+    'm4a': 'audio',
+    'flac': 'audio',
+
+    'mkv': 'video',
+    'mov': 'video',
+    'mp4': 'video',
+    'ogg': 'video',
+    'webm': 'video',
+}
+
+
+def mostrar_ayuda(error=None, exit_=False):
+    if error:
+        print(f'---- ERROR\n{error}\n----')
+    print(texto_ayuda.format(
+        archivo_script=argv[0],
+        formatos_permitidos=', '.join(formatos_permitidos.keys())
+    ))
+
+    if exit_:
+        exit()
+
+
+def crear_json_de_samples(ruta, archivo_json):
+    if not exists(ruta):
+        mostrar_ayuda('La ruta indicada no existe.', True)
+    elif not isdir(ruta):
+        mostrar_ayuda('La ruta indicada no es una carpeta.', True)
+    else:
+        la = [nomb for nomb in listdir(ruta) if isdir(f'{ruta}{nomb}')]
+        lsamples = []
+
+        for c in la:
+            ruta_c = f'{ruta}{c}'
+            lac = listdir(ruta_c)
+
+            for n, arc in enumerate(sorted(lac)):
+                ruta_c_ar = f'{ruta_c}/{arc}'
+
+                if isfile(ruta_c_ar):
+                    arc_extension = splitext(arc)[-1].strip('.')
+
+                    if arc_extension in formatos_permitidos:
+                        item =  {
+                            'type': formatos_permitidos[arc_extension],
+                            'bank': c,
+                            'n': n,
+                            'url': f'{c}/{arc}'
+                        }
+                        lsamples.append(item)
+
+        with open(join(ruta, archivo_json), 'w') as f:
+            f.write(json.dumps(lsamples, indent=4))
+
+
+if __name__ == '__main__':
+
+    if len(argv) < 2:
+        mostrar_ayuda()
+    else:
+        archivo = 'samples.json' if len(argv) < 3 else argv[2]
+        crear_json_de_samples(argv[1], archivo)
